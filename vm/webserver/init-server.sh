148adfa67cab27449a10d49076211e209dfb141e
Rulios <robert_lu20@hotmail.com>
2023-07-18 00:16:14 -0500

    vm webserver sh

---
+++ vm/webserver/init-server.sh
@@ -0,0 +1,15 @@
+#clone repo
+#git clone https://github.com/Rulios/so-semestral-speedrun.git
+
+#cd so-semestral-speedrun/webserver/
+
+./init-server.sh
+
+# Update package list and install Python 3 and Pip
+sudo apt update
+sudo apt install python3 python3-pip
+
+# Install packages from requirements.txt
+pip3 install -r requirements.txt
+
+python3 main.py
